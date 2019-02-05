f732eb2b7b8526b2ea2ba532edeaf9dedc82d0a4
Will Saffran <willh.saffran@gmail.com>
2019-02-05 05:00:12 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -55,8 +55,10 @@ end
 def runner
   welcome
   card_total = initial_round
-  until card_total > 21
-    hit?
+  while card_total < 21
+    card_total = hit?(card_total)
+    card_total = display_card_total(card_total)
   end
+  end_game(card_total)
 end
     
