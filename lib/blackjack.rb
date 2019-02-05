f3f9668cb62cba19e05cf44256950d3e4c904227
Will Saffran <willh.saffran@gmail.com>
2019-02-05 04:48:14 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -29,7 +29,7 @@ def end_game(card_total)
   puts "Sorry, you hit #{card_total}. Thanks for playing!"
 end
 
-def initial_round(deal_card)
+def initial_round
   # code #initial_round here
   sum = deal_card + deal_card
   puts display_card_total(sum)
